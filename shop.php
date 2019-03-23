1b97364edea1493dc846e448562223d945b9384f
Daniel <SpartanHedgey@gmail.com>
2019-03-23 14:00:21 +0000

    nav-fill replaced with nav-justified

--- shop.php
+++ shop.php
@@ -3,66 +3,65 @@
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>McKinley Comics Co</title>
-	<link rel="stylesheet" href="dist/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="main.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>McKinley Comics Co</title>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="styles/main.css" />
 
 </head>
 
 <body>
 
-	<!-- Banner -->
-	<div class="container-fluid">
+    <!-- Banner -->
+    <div class="container-fluid">
 
-		<!-- Logo -->
-		<div class="row site-banner">
-			<div class="col-sm-12 col-md-12 col-lg-12">
-				<a href="./index.html">
-					<img class="logo" src="img/logo.png">
-				</a>
-			</div>
-		</div>
+        <!-- Logo -->
+        <div class="row site-banner">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <a href="./index.html">
+                    <img class="logo" src="img/logo.png">
+                </a>
+            </div>
+        </div>
 
-		<!-- Nav Bar -->
-		<div class="row">
-			<div class="col-sm-12 col-md-12 col-lg-12 site-nav-bar">
-				<ul class="nav nav-pills nav-fill">
-					<!-- nav-fill makes the nav pills fill in the available space in the parent object-->
-					<li class="nav-item">
-						<a class="nav-link active" href="./index.html">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" href="#">Shop</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" href="#">About</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" href="#">Login/Sign Up</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+        <!-- Nav Bar -->
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12 site-nav-bar">
+                <ul class="nav nav-pills nav-justified">
+                    <!-- nav-justified used instead of nav-fill since it makes all the buttons the same size , rather than ones with more text being longer, while still filling the row-->
+                    <li class="nav-item">
+                        <a class="nav-link active" href="./index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Login/Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-	<div class="container page-content">
-		<div class="row">
-		<div class="col-sm-6">
+    <div class="container page-content">
+        <div class="row">
+            <div class="col-sm-3 sidebar-wrapper" style="background-color: blue; height: 200px;">
 
-		</div>
-		<div class="col-sm-6">
-			
-		</div>
-		</div>
+            </div>
+            <div class="col-sm-9 product-display-wrapper" style="background-color: red; height: 200px;">
 
-	</div>
+            </div>
+        </div>
+    </div>
 
 
-	<!--FIXME: JavaScript may not be working properly?-->
-	<script src="dist/js/jquery-3.0.0.slim.min.js"></script>
-	<script src="dist/js/bootstrap.js"></script>
+    <!--FIXME: JavaScript may not be working properly?-->
+    <script src="dist/js/jquery-3.0.0.slim.min.js"></script>
+    <script src="dist/js/bootstrap.js"></script>
 </body>
 
-</html>
+</html> 
